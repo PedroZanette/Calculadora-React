fd1edfe4ffcef1acc361755eada9a02904604777
PedroZanette <phzanette@gmail.com>
2024-07-29 16:41:45 -0300

    Versão Funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -16,18 +16,34 @@ function App() {
     setResultado(subTudo);
   }
 
+  const div = () => {
+    let subTudo = Number(num1) / Number(num2);
+    setResultado(subTudo);
+  }
+
+  const mult = () => {
+    let subTudo = Number(num1) * Number(num2);
+    setResultado(subTudo);
+  }
+
   return (
     <>
       <h1>Calculadora</h1>
       <div className="card">
 
-        <input type="number" name='num1' value={num1} onChange={(e) => setNum1(e.target.value)}/> <br />
-        <input type="number" name='num2' value={num2} onChange={(e) => setNum2(e.target.value)}/> <br />
+        <input type="number" name='num1' value={num1} onChange={(e) => setNum1(e.target.value)}/> <br /><br />
+        <input type="number" name='num2' value={num2} onChange={(e) => setNum2(e.target.value)}/> <br /><br />
         <button onClick={soma}>
           Soma  
-        </button>
+        </button> &nbsp;
         <button onClick={sub}>
           Subtração
+        </button> <br /> <br />
+        <button onClick={div}>
+          Divisão  
+        </button> &nbsp;
+        <button onClick={mult}>
+          Multiplicação
         </button>
       <p>{resultado}</p>
         <p>
